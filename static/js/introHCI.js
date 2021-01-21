1d770511b4e5c78118a3e1efa81fba7fffa61e0d
Nandini Kapa <nandinikapa@Nandinis-MBP.home>
2021-01-21 09:57:24 -0800

    Added Javascript + CSS

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,17 +2,31 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
-})
+    initializePage();
+});
 
-/*
- * Function that is called when the document is ready.
- */
 function initializePage() {
-	$("#testjs").click(function(e) {
+    $('#testjs').click(function(e) {
+		$(".jumbotron p").toggleClass("active");
+		$(".jumbotron p").fadeOut();
+		$("#testjs").text("Please wait...");
+		$("#testjs").fadeOut();
+		$(".jumbotron p").fadeIn();
 		$('.jumbotron h1').text("Javascript is connected");
-	});
+    });
+	$("a.thumbnail").click(projectClick);
+}
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+function projectClick(e) {
+    // prevent the page from reloading     
+    e.preventDefault();
+    // In an event handler, $(this) refers to     
+    // the object that triggered the event     
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
 }
